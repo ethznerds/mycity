6650f42286ac66bc393bf2c0d76f8638c7a66933
Mark Frey <freymark.dev@gmail.com>
2021-03-21 05:26:34 +0100

    Select location

--- app/lib/models/project.dart
+++ app/lib/models/project.dart
@@ -43,7 +43,8 @@ class Project {
       "name": name,
       "description": description,
       "richtext": richtext,
-      "image": image
+      "image": image,
+      "location": location
     };
 
     if(documentId != null) {
